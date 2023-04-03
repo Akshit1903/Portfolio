0b822a67b4fe271f06571b30257493eb6da2783d
Akshit Lalit <akshit.lalit@gmail.com>
2023-04-03 20:53:00 +0530

    responsive design

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -16,7 +16,6 @@ class Intro extends React.Component {
   }
   render() {
     return (
-      // <div id="home" className="intro route bg-image " style={{backgroundImage: "url("+bigImage+")"}}>
       <div id="home" className="intro route bg-image background">
         <div id="stars" />
         <div id="stars2" />
---
+++ src/components/solar-system.jsx
@@ -0,0 +1,173 @@
+import React from "react";
+
+export default function SolarSystem() {
+  return (
+    <div className="skill-mf">
+      <div className="skills-circle-wrap">
+        <div className="skills-circle">
+          {/* 5th Orbit */}
+          <div className="skill-orbit">
+            <div className="skill-planet">
+              <span>
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg"
+                  alt="mongodb"
+                  width="30"
+                  height="30"
+                />
+              </span>
+            </div>
+            <div className="skill-planet">
+              <span>
+                <img
+                  id="mysql-logo"
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg"
+                  alt="mysql"
+                  width="30"
+                  height="30"
+                />
+              </span>
+            </div>
+          </div>
+          {/* 4th Orbit */}
+          <div className="skill-orbit">
+            <div className="skill-planet">
+              <span>
+                <img
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg"
+                  alt="bootstrap"
+                  width="30"
+                  height="30"
+                />
+              </span>
+            </div>
+            <div className="skill-planet ">
+              <span>
+                <img
+                  id="figma-logo"
+                  src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg"
+                  alt="figma"
+                  width="30"
+                  height="30"
+                />
+              </span>
+            </div>
+            <div className="skill-planet">
+              <span>
+                <img
+                  id="firebase-logo"
+                  src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg"
+                  alt="firebase"
+                  width="30"
+                  height="30"
+                />
+              </span>
+            </div>
+          </div>
+          {/* 3rd Orbit */}
+          <div className="skill-orbit">
+            <div className="skill-planet">
+              <span>
+                <img
+                  src="https://www.vectorlogo.zone/logos/flutterio/flutterio-icon.svg"
+                  alt="flutter"
+                  width="30"
+                  height="30"
+                />
+              </span>
+            </div>
+            <div className="skill-planet">
+              <span>
+                <img
+                  id="dart-logo"
+                  src="https://www.vectorlogo.zone/logos/dartlang/dartlang-icon.svg"
+                  alt="dart"
+                  width="30"
+                  height="30"
+                />
+              </span>
+            </div>
+            <div className="skill-planet">
+              <span>
+                <img
+                  id="android-logo"
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/android/android-original.svg"
+                  alt="dart"
+                  width="30"
+                  height="30"
+                />
+              </span>
+            </div>
+          </div>
+          {/* 2nd Orbit */}
+          <div className="skill-orbit">
+            <div className="skill-planet">
+              <span>
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+                  alt="html5"
+                  width="30"
+                  height="30"
+                />
+              </span>
+            </div>
+            <div className="skill-planet ">
+              <span>
+                <img
+                  id="css-logo"
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+                  alt="css3"
+                  width="30"
+                  height="30"
+                />
+              </span>
+            </div>
+            <div className="skill-planet">
+              <span>
+                <img
+                  id="javascript-logo"
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+                  alt="javascript"
+                  width="30"
+                  height="30"
+                />
+              </span>
+            </div>
+          </div>
+          {/* 1st Orbit */}
+          <div className="skill-orbit">
+            <div className="skill-planet">
+              <span>
+                <img
+                  src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg"
+                  alt="c"
+                  width="30"
+                  height="30"
+                />
+              </span>
+            </div>
+            <div className="skill-planet">
+              <span>
+                <img
+                  id="cpp-logo"
+                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/cplusplus/cplusplus-original.svg"
+                  alt="cpp"
+                  width="30"
+                  height="30"
+                />
+              </span>
+            </div>
+            {/* Sun */}
+            <div className="skills-top-circle panel">
+              {/* <img
+          src="https://media2.giphy.com/media/QssGEmpkyEOhBCb7e1/giphy.gif?cid=ecf05e47a0n3gi1bfqntqmob8g9aid1oyj2wr3ds3mg700bl&rid=giphy.gif"
+          width={20}
+          height={20}
+        /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
